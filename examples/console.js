c580cd6dcb444b7cbb1738e637f159cb5fa28ea2
Robert King <hrobertking@cathmhaol.com>
2021-02-26 16:31:55 -0700

    Create console.js

---
+++ examples/console.js
@@ -0,0 +1,27 @@
+const {
+  click,
+  launch,
+} = require('../src');
+
+let browser;
+let page;
+
+launch({
+  height: 1080,
+  url: 'http://localhost:4000',
+  width: 1920,
+})
+  .then((response) => {
+    ({ browser, page } = response);
+
+    return page.screenshot({ path: 'examples/screenshot/console-load.png' });
+  })
+  .then(async () => {
+    await click({ target: '#dobDay' })
+      .then(() => page.screenshot({ path: 'examples/screenshot/console-dobDay.png' }));
+
+    await click({ target: '#dobMonth' })
+      .then(() => page.screenshot({ path: 'examples/screenshot/console-dobMonth.png' }));
+  })
+  .then(() => browser.close())
+  .catch((err) => console.log(`caught ${err}`)); // eslint-disable-line no-console
